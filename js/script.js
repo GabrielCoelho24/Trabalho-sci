6afcd1bac123f1c5bfad1dbbfda134b7ad47818e
GabrielCoelho24 <142925248+GabrielCoelho24@users.noreply.github.com>
2023-11-07 17:03:59 +0000

    Final completo

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ campoCPF.addEventListener('keyup', function(){
 })
 
 function validarFormulario(event) {
-    event.preventDefault(); // Impedir a ação padrão do formulário
+    event.preventDefault();
 
     var nome = document.forms["inscricao"]["nome"].value;
     var matricula = document.forms["inscricao"]["matrícula"].value;
@@ -19,7 +19,7 @@ function validarFormulario(event) {
     var email = document.forms["inscricao"]["email"].value;
     var turma = document.forms["inscricao"]["turma"].value;
 
-    // Validar se os campos obrigatórios estão preenchidos
+   
     if (nome === "" || matricula === "" || cpf === "" || email === "" || turma === "") {
         alert("Todos os campos obrigatórios devem ser preenchidos.");
         return false;
@@ -36,7 +36,7 @@ function salvarInformacoesLocalmente() {
     var email = document.forms["inscricao"]["email"].value;
     var turma = document.forms["inscricao"]["turma"].value;
 
-    // Salvar as informações localmente (por exemplo, no LocalStorage)
+   
     var inscricao = {
         nome: nome,
         matricula: matricula,
@@ -45,29 +45,34 @@ function salvarInformacoesLocalmente() {
         turma: turma
     };
 
-    // Converter o objeto para uma string JSON
+    
     var inscricaoJSON = JSON.stringify(inscricao);
 
-    // Armazenar os dados localmente
+    
     localStorage.setItem("inscricao", inscricaoJSON);
 
-    // Exibir uma mensagem de inscrição bem-sucedida
-    alert("Inscrição efetuada com sucesso");
+    alert('Inscrição efetuada com sucesso')
 }
 
-// Verificar se já existe a chave "total_inscritos" no Local Storage
+
 if (localStorage.getItem("total_inscritos") === null) {
-    // Se não existir, criar a chave "total_inscritos" com valor inicial 0
+  
     localStorage.setItem("total_inscritos", 0);
 }
 
-// Ao efetuar uma nova inscrição, aumentar o valor da chave "total_inscritos" em 1
+
 var totalInscritos = parseInt(localStorage.getItem("total_inscritos"));
 totalInscritos = isNaN(totalInscritos) ? 0 : totalInscritos;
 totalInscritos++;
 localStorage.setItem("total_inscritos", totalInscritos);
 
-// Para recuperar o número total de inscritos:
+
 var numeroTotalInscritos = localStorage.getItem("total_inscritos");
 
 console.log("Número total de inscritos: " + numeroTotalInscritos);
+
+function botão(){
+    if(nome === "" || matricula === "" || cpf === "" || email === "" || turma === ""){
+        
+    }
+}
